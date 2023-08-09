6f19c3cc7ba1a0ca4acac5895d10590b1e2e4342
Tetiana S <somedudepost@gmail.com>
2023-08-09 12:48:31 +0200

    Created a copy of homework 16

---
+++ homework17/src/main/java/homework17/Main.java
@@ -0,0 +1,25 @@
+package homework17;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductMarket market = new ProductMarket(
+                List.of(
+                        new Product("Vanilla Ice Cream", 12.89),
+                        new Product("Nuts Mix", 2.89),
+                        new Product("Baguette: Hawaii", 2.25),
+                        new Product("Lays KFC Chicken", 10.00),
+                        new Product("Lays Subway Teriyaki", 1.19),
+                        new Product("Bell Peppers", 1.75),
+                        new Product("Yellow Onion", 5.00),
+                        new Product("Persil Gel", 21.69)
+                )
+        );
+        System.out.println(market.getNames());
+        System.out.println(market.getNamesInAlphabeticalOrder());
+        System.out.println(market.getPricesHigherThan10());
+        System.out.println(market.getPricesLowerThan5());
+        System.out.println(market.printPrices());
+    }
+}
---
+++ homework17/src/main/java/homework17/ProductMarket.java
@@ -0,0 +1,59 @@
+package homework17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProductMarket {
+    private final List<Product> products;
+
+    public ProductMarket(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<String> getNames() {
+        List<String> names = new ArrayList<>();
+        for (Product p : products) {
+            names.add(p.getName());
+        }
+        return names;
+    }
+
+    public List<String> getNamesInAlphabeticalOrder() {
+        List<String> namesSorted = getNames();
+        Collections.sort(namesSorted);
+        return namesSorted;
+    }
+
+    public List<Double> getPricesHigherThan10() {
+        List<Double> pricesHigherThan10 = new ArrayList<>();
+        for (Product p : products) {
+            if (p.getPrice() > 10) {
+                pricesHigherThan10.add(p.getPrice());
+            }
+        }
+        return pricesHigherThan10;
+    }
+
+    public List<Double> getPricesLowerThan5() {
+        List<Double> pricesLowerThan5 = new ArrayList<>();
+        for (Product p : products) {
+            if (p.getPrice() < 5) {
+                pricesLowerThan5.add(p.getPrice());
+            }
+        }
+        return pricesLowerThan5;
+    }
+
+    public String printPrices() {
+        return String.join(", ", getStringPrices());
+    }
+
+    private List<String> getStringPrices() {
+        List<String> price = new ArrayList<>();
+        for (Product p : products) {
+            price.add(String.valueOf(p.getPrice()));
+        }
+        return price;
+    }
+}
