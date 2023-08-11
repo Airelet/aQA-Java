5b75c9e1a3d2d28535fd1140beaaf73e900177c0
Tetiana S <somedudepost@gmail.com>
2023-08-11 17:49:25 +0200

    made a copy of homework 17

---
+++ homework18/src/main/java/homework18/Product.java
@@ -0,0 +1,25 @@
+package homework18;
+
+public class Product implements Comparable<Product> {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public int compareTo(Product anotherProduct) {
+        return Double.compare(getPrice(), anotherProduct.getPrice());
+    }
+}
